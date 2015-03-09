522775314d56188e5bf3322c6eaa3d9d8c088179
katrinnilsson <nilsson.katrin@gmail.com>
2015-03-09 22:09:23 +0100

    Some more things in progress

--- app/src/main/java/iprog/group7/agendabuilder/android/view/DayView.java
+++ app/src/main/java/iprog/group7/agendabuilder/android/view/DayView.java
@@ -1,27 +1,41 @@
 package iprog.group7.agendabuilder.android.view;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.Observable;
+import java.util.Observer;
+
 import iprog.group7.agendabuilder.android.R;
+import iprog.group7.agendabuilder.model.AgendaModel;
 
 /**
  * View for day schedule, horizontal scrollable
  */
-public class DayView {
+public class DayView implements Observer {
 
     View view;
+    AgendaModel model;
+    Button addDay;
 
-    public DayView(View view) {
+    public DayView(View view, AgendaModel model) {
 
         this.view = view;
+        this.model = model;
+        model.addObserver(this);
         TextView whichDayTitle = (TextView) view.findViewById(R.id.which_day_title);
         EditText startTime = (EditText) view.findViewById(R.id.start_time);
         TextView endTime = (TextView) view.findViewById(R.id.end_time);
         TextView totalLength = (TextView) view.findViewById(R.id.total_length);
         RelativeLayout boxDayLayout = (RelativeLayout) view.findViewById(R.id.box_day_layout);
+        addDay = (Button) view.findViewById(R.id.add_day_button);
+
+    }
+
+    public void update(Observable observable, Object object) {
 
     }
 
